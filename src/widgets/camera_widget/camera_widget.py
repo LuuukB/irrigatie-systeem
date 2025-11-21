f292e293b366e0d9df435c9aeecf9e5b77bf3362
LuuukB <158037306+LuuukB@users.noreply.github.com>
2025-11-21 11:06:40 +0100

    made a camera widget that only needs a camera to show the live feed

---
+++ src/widgets/camera_widget/camera_widget.py
@@ -0,0 +1,47 @@
+import os
+import asyncio
+
+from src.camera.i_camera_handler import ICameraHandler
+
+# Must come before kivy imports
+os.environ["KIVY_NO_ARGS"] = "1"
+
+from kivy.input.providers.mouse import MouseMotionEvent  # noqa: E402
+from kivy.lang.builder import Builder  # noqa: E402
+from kivy.uix.widget import Widget  # noqa: E402
+
+class CameraWidget(Widget ):
+    def __init__(self, **kwargs):
+        super(CameraWidget,self ).__init__(**kwargs)
+        self.texture : Texture = None
+        self.camera = Optional[IcameraHandler]
+        self.running : bool = True
+
+        Builder.load_file(os.path.join(os.path.dirname(__file__), "camera_widget.kv"))
+
+    async def start_stream(self, camera : ICameraHandler):
+        self.camera = camera
+
+        while self.running:
+
+            frame = await self.camera.get_frame()
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            if self.texture is None:
+                self.texture = Texture.create(
+                    size=(frame.shape[1], frame.shape[0]), icolorfmt="rgb"
+                )
+                self.texture.flip_vertical()
+
+            self.texture.blit_buffer(
+                bytes(frame.data),
+               colorfmt="rgb",
+               bufferfmt="ubyte",
+               mipmap_generation=False,
+            )
+
+            self.root.ids.image.texture = self.texture
+            await asyncio.sleep(0.01)
+
+    async def stop_stream(self):
+        self.running = False
