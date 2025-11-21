946eed93b2542cbd0e610a66c8d9542dcc5dee95
LuuukB <158037306+LuuukB@users.noreply.github.com>
2025-11-21 12:00:43 +0100

    need to wait for root to be set in camera_widget

--- src/widgets/camera_widget/camera_widget.py
+++ src/widgets/camera_widget/camera_widget.py
@@ -26,6 +26,9 @@ class CameraWidget(Widget ):
         starts a loop that gets frames from the given camera,
         turns them into a texture and fils the Image with the texture
         """
+        while self.root is None:
+            await asyncio.sleep(0.01)
+
         self.camera = camera
 
         while self.running:
