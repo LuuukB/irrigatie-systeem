4ac4ada13c2f66500f0f341083840e55ecf84c29
LuuukB <158037306+LuuukB@users.noreply.github.com>
2025-11-26 12:27:30 +0100

    changed home layout.

--- src/screens/home_screen.py
+++ src/screens/home_screen.py
@@ -17,22 +17,15 @@ class HomeScreen(Screen):
         super().__init__(**kwargs)
         self.vm = HomeModel()
         self.task : asyncio.Task = None
-        #asyncio.create_task(self.stream_cameras())
-
-    #def on_leave(self):
-    #    self.task.cancel()
-
-    def increase_counter(self):
-        self.vm.increase()
-        self.ids.counter_label.text = f"Teller: {self.vm.counter}"
-
-    async def stream_cameras(self):
-
-        camera_factory = CameraFactory()
-        camera_widget: CameraWidget = self.ids["oak0"]
-        camera_factory.add_camera_offline("video")
-        await camera_factory.start_all()
-        print("start")
-        self.task = asyncio.create_task(
-            camera_widget.stream_camera(camera_factory.get_camera("video"))
-        )
+        self.start_task = False
+    
+    
+    def start_stop(self):
+        self.start_task = not self.start_task
+        print(self.start_task)
+        if self.start_task:
+            print("go")
+            self.ids.start_stop_btn.text = "stop"
+        else:
+            print("stop")
+            self.ids.start_stop_btn.text = "start"
