2150150a7364a96cf82474815c58a6596e50bbef
LuuukB <158037306+LuuukB@users.noreply.github.com>
2026-01-08 09:44:57 +0100

    check to see what current speed returns

--- src/processing/point_handler.py
+++ src/processing/point_handler.py
@@ -94,7 +94,8 @@ class PointHandler:
         self.old_time = current_time
         
         current_speed = self.can_bus.get_speed()
-        average_speed = (current_speed + self.old_speed) / 2
+        logger.debug(f"current speed: {current_speed}")
+        average_speed = (float(current_speed) + self.old_speed) / 2
         self.old_speed = current_speed
 
         distance_traveled = average_speed * forgone_time * 1000
