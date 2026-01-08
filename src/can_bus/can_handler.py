da1fc213671f22b21fd701d927b895cae26ba890
LuuukB <158037306+LuuukB@users.noreply.github.com>
2026-01-08 14:06:58 +0100

    speed doesnt seem to change

--- src/can_bus/can_handler.py
+++ src/can_bus/can_handler.py
@@ -71,6 +71,7 @@ class CanHandler(ICanHandler):
             with self.lock:
                 self.speed = measured_speed # m/s
                 logger.info("changed speed")
+            await asyncio.sleep(1)
 
     async def get_speed(self):
         while True:
