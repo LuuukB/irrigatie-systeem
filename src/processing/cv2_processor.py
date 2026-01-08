35f4afaac4092593b9e9a97445d056c2ffb2303b
LuuukB <158037306+LuuukB@users.noreply.github.com>
2026-01-08 11:04:58 +0100

    thread stops after one time

--- src/processing/cv2_processor.py
+++ src/processing/cv2_processor.py
@@ -15,9 +15,10 @@ class Cv2Processor:
     def get_contours(self, frame, img_filter: ImageFilter = None):
         logger.debug("get contours")
         img = frame.copy()
+
         if img_filter:
             frame = img_filter.apply_filter(frame)
-
+        logger.debug("aplied filter")
         frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         ret, binary = cv2.threshold(frame_gray, 1, 150, cv2.THRESH_BINARY)
@@ -30,6 +31,8 @@ class Cv2Processor:
         contours, hierarchy = cv2.findContours(
             binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
         )
+
+        logger.debug("found contours")
 
         #cv2.drawContours(image=frame, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=2,
          #                lineType=cv2.LINE_AA)
