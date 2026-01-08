b649924115e32c560434750d34e245201450a21d
LuuukB <158037306+LuuukB@users.noreply.github.com>
2026-01-08 13:44:13 +0100

    idk what happend but canbus is giving error

--- src/processing/point_handler.py
+++ src/processing/point_handler.py
@@ -70,8 +70,8 @@ class PointHandler:
                         # if distance < 2 (staat niet vast is idee) sent give water message (amount depends on how many setups)
                         if crop.distance <= 20:
                             #send give water message
-                            await self.can_bus.send_to_microcontroller(message = SetupPdo(command=2, amount=crop.x), destination = 0x300 + setup)
-                            await self.can_bus.send_to_microcontroller(message = SetupPdo(command = 1, amount = int(500 / crop.setup_amount)), destination = 0x300 + setup)
+                            await self.can_bus.send_to_microcontroller(message = SetupPdo(command=2, amount=crop.x), id = 0x300 + setup)
+                            await self.can_bus.send_to_microcontroller(message = SetupPdo(command = 1, amount = int(500 / crop.setup_amount)), id = 0x300 + setup)
                             continue
                         else:
                             new_crops.append(crop)
