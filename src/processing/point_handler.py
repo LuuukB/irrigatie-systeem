d2aa6a4aab1525c23e4fb81addeda514b27f4754
LuuukB <158037306+LuuukB@users.noreply.github.com>
2026-01-15 10:27:47 +0100

    logger for check

--- src/processing/point_handler.py
+++ src/processing/point_handler.py
@@ -66,6 +66,8 @@ class PointHandler:
                     self.setups[setup + (i - 1)].append(crop)
                     print(f"added crop {crop} to setup {setup + (i - 1)}")
                     logger.info(f"camera {camera_number} added crop {crop} to setup {setup + (i - 1)}")
+                else:
+                    logger.debug(f"crop already exists in setup {setup + (i - 1)}")
         else:
             print("out of scope")
             print(setup, setup_amount, x)
