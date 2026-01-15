bb29ad9260d39d2a3e120a10d3a195f3c408324c
LuuukB <158037306+LuuukB@users.noreply.github.com>
2026-01-15 09:23:13 +0100

    deleted loggings

--- src/processing/cv2_processor.py
+++ src/processing/cv2_processor.py
@@ -51,11 +51,9 @@ class Cv2Processor:
                 draw_midpoints = self.tracker.track_contours(centroids)
                 for iterations, c, cx, cy in draw_midpoints:
                     if iterations < 15:
-                        logger.debug(f"found points with {iterations} iterations")
                         cv2.circle(img, (cx, cy), 4, (0, 0, 255), -1)
                     elif iterations == 15:
                         self.point_handler.handle_point(cx, cy, camera_number)
-                        logger.debug(f"Camera {camera_number} is done")
             except Exception as e:
                 print(e)
 
