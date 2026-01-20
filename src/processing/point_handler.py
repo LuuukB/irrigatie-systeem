42c480cea9078945a60d7d8bec8d8cd83c84f704
LuuukB <158037306+LuuukB@users.noreply.github.com>
2026-01-20 13:55:21 +0100

    calculate x to sent to microcontroller

--- src/processing/point_handler.py
+++ src/processing/point_handler.py
@@ -19,7 +19,9 @@ class Crop:
 class PointHandler:
     amount_of_cameras = 2
     amount_of_stroken = 7
+    amount_of_setups = 4
     screen_width = 1920
+    quarter_of_Screen = screen_width // amount_of_cameras
     pixels_per_cm = 1920 / 86
     camera_px = 86 * 22.33
     overlap_px = 30 * 22.33
@@ -38,7 +40,6 @@ class PointHandler:
         setup = None
         setup_amount = 0
 
-        """adds pixels to the camera compared to where it is under the robot."""
         logger.debug(f"Camera {camera_number} has x {x}")
         if camera_number > 0:
             width = x + (self.screen_width * camera_number - self.overlap_px * camera_number)
@@ -48,7 +49,7 @@ class PointHandler:
         step = self.total_amount_of_pixels // self.amount_of_stroken
         logger.info(f"{step} {width}")
 
-        """check in witch setup_list point should go"""
+        #check in witch setup_list point should go
         for i in range(self.amount_of_stroken):
             if ((i-1)*step) < width < (i * step):
                 print("jahoor setup gevonden")
@@ -59,13 +60,15 @@ class PointHandler:
         distance = self.get_distance(y, setup_amount)
 
         # stamp with amount of setup_lists
-        """"check in witch setup_list point should go and if its not already there"""
+        # add to correct setup
         if setup is not None and setup_amount > 0:
             crop = Crop(x = width, distance = distance, setup_amount = setup_amount)
             for i in range(setup_amount):
                 if not self._check_crop(crop, self.setups[setup + (i - 1)]):
+                    point = width - self.quarter_of_Screen * (setup - 1)
+                    crop.x = point // self.quarter_of_Screen * 150
                     self.setups[setup + (i - 1)].append(crop)
-                    print(f"added crop {crop} to setup {setup + (i - 1)}")
+                    print(f"added crop {crop} to setup {setup + (i - 1)} with x {x}")
                     logger.info(f"camera {camera_number} added crop {crop} to setup {setup + (i - 1)}")
                 else:
                     logger.info(f"skipped crop {crop}")
@@ -73,14 +76,15 @@ class PointHandler:
             print("out of scope")
             print(setup, setup_amount, x)
 
+    #def loop die constand de lijst checkt op waar welk punt is
     async def check_distances(self):
-        """A loop that constantly checks the list to see where each point is."""
         self.old_time = time.monotonic()
         self.old_speed = 0.2
         while True:
 
             if any(crops for crops in self.setups.values()):
                 #check_distance
+                #logger.debug(f"found crops")
                 distance_traveled = await self.calculate_distance_traveled()
                 #loops trough every point in the list and substracts traveld distance from there own distance
                 for setup, crops in self.setups.items():
@@ -89,18 +93,19 @@ class PointHandler:
                     for crop in crops:
                         crop.distance -= distance_traveled
                         logger.debug(f"{crop.distance}")
-                        #check if crop is far enough to give water
+                        # if distance < 2 (staat niet vast is idee) sent give water message (amount depends on how many setups)
                         if crop.distance <= 20:
+                            #send give water message
                             await self.can_bus.send_to_microcontroller(message = RawCanbusMessage(
                                 data = SetupPdo(
-                                    command=2,
+                                    command=1,
                                     amount=int(crop.x)).to_can_data(),
                                 id = 0x300 + setup))
                             logger.debug("done sending")
 
                             await self.can_bus.send_to_microcontroller(message = RawCanbusMessage(
                                 data = SetupPdo(
-                                    command = 1,
+                                    command = 0,
                                     amount = int(500 / crop.setup_amount)).to_can_data(),
                                 id = 0x300 + setup))
                             continue
@@ -117,7 +122,7 @@ class PointHandler:
             # wait...
             await asyncio.sleep(0.1)
 
-    """method that calculates the distance traveled since last update"""
+    #def hulp methode die met gegeven snelheid en huidige punt nieuwe x coordinaat berekend
     async def calculate_distance_traveled(self):
         #gets the current time
         #calculates past distance in given stime stack
@@ -132,19 +137,18 @@ class PointHandler:
 
         return distance_traveled
 
-    """initial method to set distance for point to where water needs to be given"""
+    # initial method to set distance for point to where water needs to be given
     def get_distance(self, y, amount_of_setups):
         #gets the y coordinate and calculates the distance to the end point
-        distance = (1920 - y) * 0.40 #pixel to milimeter conversion
-        #+ distance of camera tot the point were the water needs to be given
+        distance = (1920 - y) * 0.40 #pixel naar milimeter conversion
+        #+ afstand van onderkant camera tot daadwerkelijke punt van watergeven
         distance += 800
         #changes end point to amount_of _setups because turning changes the distance
         if amount_of_setups == 1:
             return distance
         else:
-            return distance - 30 # less distance since giving water to multiple plants is closer
+            return distance - 30 #extra afstand bij turnen
 
-    """check if crop already exists"""
     def _check_crop(self, new_crop: Crop, setup, tolerance=50):
         for crop in setup:
             if (
