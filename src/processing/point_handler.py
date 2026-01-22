d1dadc0dcd88cd8dd997dcfc4a6b9e746bf30e30
LuuukB <158037306+LuuukB@users.noreply.github.com>
2026-01-22 13:27:27 +0100

    added log

--- src/processing/point_handler.py
+++ src/processing/point_handler.py
@@ -47,12 +47,13 @@ class PointHandler:
         else:
             width = x
         step = self.total_amount_of_pixels // self.amount_of_stroken
-        #logger.info(f"{step} {width}")
+        logger.info(f"{step} {width}")
 
         #check in witch setup_list point should go
         for i in range(self.amount_of_stroken):
             if (i * step) < width < (i+1 * step):
                 #print("jahoor setup gevonden")
+                logger.debug(f"found setup {i} {width}")
                 setup = i // 2 + 1
                 setup_amount = i % 2 + 1
 
