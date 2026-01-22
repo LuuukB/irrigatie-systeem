f2534ba760d18c062c4aa7569a70d8cc15e5656b
LuuukB <158037306+LuuukB@users.noreply.github.com>
2026-01-22 13:37:05 +0100

    added log

--- src/processing/point_handler.py
+++ src/processing/point_handler.py
@@ -51,7 +51,8 @@ class PointHandler:
 
         #check in witch setup_list point should go
         for i in range(self.amount_of_stroken):
-            if (i * step) < width < (i+1 * step):
+            logger.debug(f" {i * step} <= width < {i+1 * step}")
+            if (i * step) <= width < (i+1 * step):
                 #print("jahoor setup gevonden")
                 logger.debug(f"found setup {i} {width}")
                 setup = i // 2 + 1
