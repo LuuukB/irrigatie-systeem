a409fe01c8315647c7da2375d5565bfccd168204
LuuukB <158037306+LuuukB@users.noreply.github.com>
2026-01-23 11:59:48 +0100

    add ofsset instead of remove

--- src/processing/point_handler.py
+++ src/processing/point_handler.py
@@ -95,7 +95,7 @@ class PointHandler:
                 logger.debug(f"nineth of the screen: {self.nineth_of_screen}")
                 point = width - self.third_of_screen * (setup - 1)
                 logger.debug(f"point: {point}")
-                point_with_offset = point - self.nineth_of_screen * (setup - 1)
+                point_with_offset = point + self.nineth_of_screen * (setup - 1)
                 logger.debug(f"point with offset: {point_with_offset}")
                 crop.location = max(
                     0, 150 - (point_with_offset / self.third_of_screen * 150)
